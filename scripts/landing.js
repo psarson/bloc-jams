afb52956bbcec1fcd1cab1c8002a6f5ab77ea86f
Patrick Sarson <sarsonmedia@gmail.com>
2016-06-09 13:41:29 -0400

    Add Assignment 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,13 @@
 var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {
-        var revealPoint = function(ind) {
-            points[ind].style.opacity = 1;
-            points[ind].style.transform = "scaleX(1) translateY(0)";
-            points[ind].style.msTransform = "scaleX(1) translateY(0)";
-            points[ind].style.WebkitTransform = "scaleX(1) translateY(0)";
-        };
-    for(var i = 0; i < points.length; i++){
-        revealPoint(i);
-    } 
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+var animatePoints = function(point) {
+    forEach(point, revealPoint);
 }; 
 
 window.onload = function() {
