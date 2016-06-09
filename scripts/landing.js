c12e38da8505ecb9e6be535920c570aa40a0161e
Patrick Sarson <sarsonmedia@gmail.com>
2016-06-09 10:40:13 -0400

    Add Checkpoint 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,5 +1,6 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+ 
+ var animatePoints = function(points) {
         var revealPoint = function(ind) {
             points[ind].style.opacity = 1;
             points[ind].style.transform = "scaleX(1) translateY(0)";
@@ -11,4 +12,17 @@ var animatePoints = function() {
     } 
 }; 
 
-animatePoints();
+window.onload = function() {
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+     window.addEventListener('scroll', function(event) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+ }
